920c69fe8bcc79b62e694c97dca5aaa7e8ee3023
Jack Carey <jackscmail@gmail.com>
2025-01-20 14:29:35 +0900

    update setup script

--- setup.sh
+++ setup.sh
@@ -8,11 +8,7 @@ brew bundle install --file ~/.config/brewfile/Brewfile-core
 
 # Place .zshenv to change zdotdir
 echo "Setting up zsh..."
-ln -s .config/zsh/startup/.zshenv ${HOME}/.zshenv
-
-# Set Prezto upstream
-cd .config/zsh/.zprezto
-git remote add upstream git@github.com:sorin-ionescu/prezto.git
+ln -s .config/zsh/.zshenv ${HOME}/.zshenv
 
 # Make tmux scripts executable
 chmod +x ~/.config/tmux/scripts/*
