2865a0813a0df821e98a44aba367846da8867406
Jack Carey <jackscmail@gmail.com>
2025-03-10 18:18:23 +0900

    optimise media script slightly

--- tmux/scripts/media-info.sh
+++ tmux/scripts/media-info.sh
@@ -3,6 +3,7 @@
 final_info=""
 available_width=$((($1 - 80) >> 1))                                # approximation for around half of available middle width
 available_width=$(($available_width > 45 ? 45 : $available_width)) # Limit to 45
+((available_width <= 0)) && exit 0                                 # exit if no space
 player=$(fastfetch -c ~/.config/fastfetch/tmux/player.jsonc --player-format "{1}")
 
 if [ -n "$player" ]; then
@@ -19,11 +20,9 @@ if [ -n "$player" ]; then
 	esac
 	final_info="${final_info}$(fastfetch -c ~/.config/fastfetch/tmux/media.jsonc --media-format "{3} - {1}")"
 
-	if ((available_width > 0)); then
-		if ((${#final_info} > available_width)); then
-			echo -nE "${final_info:0:available_width}…"
-		else
-			echo -nE $final_info " "
-		fi
+	if ((${#final_info} > available_width)); then
+		echo -nE "${final_info:0:available_width}…"
+	else
+		echo -nE $final_info " "
 	fi
 fi
