a430451a3ef2c71b6dcac002c9390959d04f3053
Jack <jackscmail@gmail.com>
2025-04-08 18:46:46 +0900

    finished basic options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -11,16 +11,29 @@ opt.cursorline = true
 opt.tabstop = 3
 opt.shiftwidth = 3
 
+opt.breakindent = true
+
+-- After indent plugin
+-- opt.list = true
+-- opt.listchars = { tab = "▏ ", trail = "·", extends = "❭", precedes = "❬", nbsp = "+" }
+
+opt.scrolloff = 8
+
 opt.ignorecase = true
 opt.smartcase = true
 
 opt.splitbelow = true
 opt.splitright = true
 
+opt.confirm = true
+
 opt.spelllang = { "en_au", "cjk" }
 opt.termguicolors = true
 opt.clipboard = "unnamedplus"
 opt.wildmode = "longest:full,full"
+
+opt.updatetime = 350
+opt.timeoutlen = 500
 
 -------------------
 -- General Mappings
