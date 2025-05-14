851ec2424905e454be6cf9b2c352f599902e7666
Jack <jackscmail@gmail.com>
2025-05-14 18:20:37 +0900

    Add basic set of picker keybinds

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
+	---@type snacks.Config
 	opts = {
 		indent = {
 			indent = {
@@ -16,21 +17,45 @@ return {
 				enabled = false,
 			},
 		},
+		picker = {
+			sources = {
+				files = {
+					hidden = true,
+				},
+				grep = {
+					hidden = true,
+				},
+				commands = {
+					layout = {
+						preset = "vscode"
+					}
+				},
+				help = {
+					confirm = {
+						action = "help",
+						cmd = "vsplit",
+					}
+				},
+				pickers = {
+					layout = {
+						preset = "select"
+					}
+				}
+			}
+		}
 	},
 	keys = {
 		-- Pickers
-		{ "<leader><space>", function()
-			Snacks.picker.files({
-				hidden = true,
-				dirs = vim.lsp.buf.list_workspace_folders()
-			})
-		end, desc = "Find all files" },
-		{ "<leader>/", function()
-			Snacks.picker.grep({
-				hidden = true,
-				dirs = vim.lsp.buf.list_workspace_folders()
-			})
-		end, desc = "Grep all files" },
+		{ "<leader><space>", function() Snacks.picker.files({ dirs = vim.lsp.buf.list_workspace_folders() }) end, desc = "Find all files" },
+		{ "<leader>/", function() Snacks.picker.grep({ dirs = vim.lsp.buf.list_workspace_folders() }) end, desc = "Grep all files" },
+		{ "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+
+		{ "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+
+		{ "<leader>pc", function() Snacks.picker.commands() end, desc = "Commands" },
+		{ "<leader>pp", function() Snacks.picker.projects() end, desc = "Projects" },
+		{ "<leader>pH", function() Snacks.picker.help() end, desc = "Help pages" },
+		{ "<leader>pr", function() Snacks.picker.resume() end, desc = "Resume last picker" },
 		{ "<leader>pa", function() Snacks.picker() end, desc = "All pickers" }
 	}
 }
