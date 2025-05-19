4a2fd372cfe65fa326dc71312473f276ab546e0d
Jack <jackscmail@gmail.com>
2025-05-20 03:15:36 +0900

    Add basedpyright, write boiler

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,7 +1,14 @@
--- Diagnostic setup
+-- Diagnostic setup (settings)
 
 -- Enable all lsp servers in nvim/lsp
 local servers = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true)):map(function(file)
 	return file:match("/lsp/(.+)%.lua")
 end):totable()
 vim.lsp.enable(servers)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("my_lspattach", {}),
+	desc = "Configure LSP",
+	callback = function(args)
+	end
+}
