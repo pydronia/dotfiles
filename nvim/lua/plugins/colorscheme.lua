2c857a71a23af8bbda6607d15f25703313b2edd0
Jack <jackscmail@gmail.com>
2025-05-21 18:00:00 +0900

    Add tokyonight scheme for testing

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,38 +1,44 @@
 return {
-	"craftzdog/solarized-osaka.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		local hslutil = require("solarized-osaka.hsl")
-		local hsl = hslutil.hslToHex
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local hslutil = require("solarized-osaka.hsl")
+			local hsl = hslutil.hslToHex
 
-		require("solarized-osaka").setup({
-			transparent = false,
+			require("solarized-osaka").setup({
+				transparent = false,
 
-			-- Color overrides
-			on_colors = function(colors)
-				-- Fix dark colors
-				local darker = hsl(192, 100, 3)
-				colors.base04 = darker
-				colors.bg_popup = darker
-				colors.bg_sidebar = darker
-				colors.bg_float = darker
-			end,
+				-- Color overrides
+				on_colors = function(colors)
+					-- Fix dark colors
+					local darker = hsl(192, 100, 3)
+					colors.base04 = darker
+					colors.bg_popup = darker
+					colors.bg_sidebar = darker
+					colors.bg_float = darker
+				end,
 
-			-- Custom Highlights
-			---@param hl table
-			on_highlights = function(hl, c)
-				hl.MatchParen = { fg = c.red100, bold = true }
+				-- Custom Highlights
+				---@param hl table
+				on_highlights = function(hl, c)
+					hl.MatchParen = { fg = c.red100, bold = true }
+					hl["@module"] = { link = "Directory" }
 
-				-- Treesitter Context
-				hl.TreesitterContext = { bg = c.base03 }
-				hl.TreesitterContextBottom = { sp = c.base01, underline = true }
+					-- Treesitter Context
+					hl.TreesitterContext = { bg = c.base03 }
+					hl.TreesitterContextBottom = { sp = c.base01, underline = true }
 
-				-- Indent guides
-				hl.SnacksIndent = { fg = c.base02, nocombine = true}
-			end
-		})
+					-- Indent guides
+					hl.SnacksIndent = { fg = c.base02, nocombine = true}
+				end
+			})
 
-		vim.cmd.colorscheme("solarized-osaka")
-	end,
+			vim.cmd.colorscheme("solarized-osaka")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+	}
 }
