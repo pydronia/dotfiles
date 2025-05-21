d8f40a71427df37a83061fee4bc56f820f3f02f2
Jack <jackscmail@gmail.com>
2025-05-21 17:20:24 +0900

    Fix todo highlighting

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,6 +5,9 @@ return {
 
 		-- Add custom config here or in after/lsp
 		-- NOTE: Merging may be changed in the future: https://github.com/neovim/neovim/issues/33577
+		vim.api.nvim_set_hl(0, "@lsp.type.comment.lua", {}) -- Disable comment semantic tokens for lua
+
+
 		-- Enable LSP servers (see :h lspconfig-all)
 		vim.lsp.enable({
 			"lua_ls",
