109fb6c6c4c606b76fd12ecc887ce8aceab745fa
Jack <7018930+pydronia@users.noreply.github.com>
2025-09-19 17:28:33 +0900

    fix tmux media script

--- tmux/scripts/media-info.sh
+++ tmux/scripts/media-info.sh
@@ -4,25 +4,33 @@ final_info=""
 available_width=$((($1 - 80) >> 1))                                # approximation for around half of available middle width
 available_width=$(($available_width > 45 ? 45 : $available_width)) # Limit to 45
 ((available_width <= 0)) && exit 0                                 # exit if no space
-player=$(fastfetch -c ~/.config/fastfetch/tmux/player.jsonc --player-format "{1}")
+{
+	read -r player
+	read -r playing
+	read -r media
+} < <(fastfetch -c ~/.config/fastfetch/tmux/media.jsonc)
 
 if [ -n "$player" ]; then
-	case $player in
-	Spotify)
-		final_info='󰓇 '
-		;;
-	Firefox)
-		final_info='󰈹 '
-		;;
-	*)
-		final_info=' '
-		;;
-	esac
-	final_info="${final_info}$(fastfetch -c ~/.config/fastfetch/tmux/media.jsonc --media-format "{3} - {1}")"
+	if [[ "$playing" == "Paused" ]]; then
+		final_info=' '
+	else
+		case $player in
+		Spotify)
+			final_info='󰓇 '
+			;;
+		Firefox)
+			final_info='󰈹 '
+			;;
+		*)
+			final_info=' '
+			;;
+		esac
+	fi
+	final_info="${final_info}${media}"
 
 	if ((${#final_info} > available_width)); then
 		echo -nE "${final_info:0:available_width}…"
 	else
-		echo -nE $final_info " "
+		echo -nE "$final_info" " "
 	fi
 fi
